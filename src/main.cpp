3c90e055d607e23f36f5857802c15a294e419927
Had3s-styx <259622123+Had3s-styx@users.noreply.github.com>
2026-09-21 14:42:35 -0500

    testing the servo sweep using a for loop

--- src/main.cpp
+++ src/main.cpp
@@ -18,30 +18,14 @@ void setup() // Runs one time when the board turns on.
 
 void loop() 
 {
-  // Step 1
-  radarServo.write(90);           // set the servo angle
-  delay(20);                      // delay while the servo settles
-  digitalWrite(TRIG_PIN, LOW);    // Makes sure the Trig Pin starts at a quiet state of 0v
-  delayMicroseconds(2);           // Short microsecond delay
-  digitalWrite(TRIG_PIN, HIGH);   // HIGH = on, 5V starts the ping 
-  delayMicroseconds(10);          // small delay in microseconds
-  digitalWrite(TRIG_PIN, LOW);    // Turns off the sensor.
-
-  // Step 2
-  // times how long the echo takes to bounce back.
-  // pulseIn is like a stopwatch: starts when echo turns on and stops when it turns off
-  long duration = pulseIn(ECHO_PIN, HIGH);    
-
-  // Step 3
-  // Distance = speed x time
-  // sound travels at about 343 meters per second. in centimeters per microsecond thats 0.0343
-  // using distance = duration * 0.0343 / 2
-  float distance = duration * 0.0343 / 2; // time x speed of sound cut in half for one-way
-
-  // Step 4 : show the distance label, the distance and unit of measurement
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-  delay(5000);
-
+  for (int angle = 0; angle <=180; angle += 2)
+  {
+    radarServo.write(angle);
+    delay(15);
+  }
+  for (int angle = 180; angle >= 0; angle -= 2)
+  {
+    radarServo.write(angle);
+    delay(15);
+  }
 }
