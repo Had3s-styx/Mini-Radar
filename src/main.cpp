3def850aa09e81ae47dbfa590b316e84bd54a5fd
Had3s-styx <259622123+Had3s-styx@users.noreply.github.com>
2026-09-21 18:02:03 -0500

    Adding and testing a oled display

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>        // grabs the arduino dictionary so the board knows serial and delay
-#include <Servo.h>         
+#include <Servo.h>
+#include <U8g2lib.h>
+
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C display(U8G2_R0, U8X8_PIN_NONE);
 
 // Names set for the pins the wires are plugged into
 const int TRIG_PIN = 9;     // sets the trig pin variable to 9
@@ -10,6 +13,17 @@ Servo radarServo;
 
 void setup() // Runs one time when the board turns on.
 {
+  display.begin();
+  display.firstPage();
+  do 
+  {
+    display.drawCircle(64, 63, 60, U8G2_DRAW_UPPER_RIGHT | U8G2_DRAW_UPPER_LEFT);
+    display.drawCircle(64, 63, 40, U8G2_DRAW_UPPER_RIGHT | U8G2_DRAW_UPPER_LEFT);
+    display.drawCircle(64, 63, 20, U8G2_DRAW_UPPER_RIGHT | U8G2_DRAW_UPPER_LEFT);
+    display.drawHLine(4, 63, 121);
+
+  } while (display.nextPage());
+
   Serial.begin(9600);
   pinMode(TRIG_PIN, OUTPUT);      // Trig pin SENDS signlas out.
   pinMode(ECHO_PIN, INPUT);       // Echo Pin LISTENS for signals.
